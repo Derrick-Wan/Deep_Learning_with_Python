df961614b409c187299881cf418a109a10759d22
Derrick-Wan <scuderrickwan@gmail.com>
2019-04-25 20:34:29 +0800

    MNIST, the Hello world of Deep Learning.

--- Chapter_2.py
+++ Chapter_2.py
@@ -7,4 +7,7 @@ from keras import layers
 network = models.Sequential()
 network.add(layers.Dense(512, activation='relu', input_shape=(28 * 28,)))
 network.add(layers.Dense(10, activation='softmax'))
-
+network.compile(optimizer='rmsprop',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+network.fit(train_images, train_labels, epochs=5, batch_size=128)
